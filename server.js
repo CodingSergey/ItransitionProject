cd5ca6309a4469dbdf3d0d29ee20a9793c2fe480
CodingSergey <a.b.games.business@gmail.com>
2022-10-01 15:04:46 +0200

    Working with Postman??

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
+const cors = require("cors");
 let port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get("/", (req,res)=>{
     console.log("requested");
     res.send({status:"ok"});
 })
-
-app.listen(port);
+ 
+app.listen(port, ()=>{
+    console.log("Listening on port:", port);
+});
