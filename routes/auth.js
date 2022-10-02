eccb0e31b8ed1a2adda2a6764a83d80bc69bca59
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 11:36:02 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {body, validationResult} = require("express-validator");
-const {createUser} = require("../authorization/validation");
+const {createUser} = require("../userManagement/createUser");
 const Account = require("../models/Account");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
