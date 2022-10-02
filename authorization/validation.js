208055779ef38770c3258ff9c73c64cbd477c64c
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 11:04:51 +0200

    Testing

--- authorization/validation.js
+++ authorization/validation.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const createUser = async (_username, _email, _password) => {
     await Account.create({
         username: _username,
-        password: bcrypt.hash(_password, 10),
+        password: await bcrypt.hash(_password, 10),
         email: _email,
         admin: false,   
         blocked: false
