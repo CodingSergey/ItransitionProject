aae7deab8fdc52a3fd9d45296fd0eab2f9f67095
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 09:20:10 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const {body, validationResult} = require("express-validator");
 const router = express.Router();
 const Account = require("../models/Account");
 
-router.post("/register", (req,res) => {
-    
+router.post("/register", 
+    body("email").isEmail(),
+    body("password").isLength({min: 6}),
+(req,res) => {
+    const {username, email, password} = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+    createUser(username,email,password);
+    res.send({status: "ok"});
 })
-
+function createUser() {
+    
+}
 module.exports = router;
