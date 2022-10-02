b199f98aa8bd0c1e78253f18d7c667cec81c5520
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 08:40:41 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.send({status:"cock"});
-})
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const auth  = require("./routes/auth");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://alex:1234@cluster0.0nmnmgi.mongodb.net/?retryWrites=true&w=majority")
+
 let port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/", auth);
+app.use("/auth", auth);
 app.listen(port, ()=>{
     console.log("Listening on port:", port);
 });
