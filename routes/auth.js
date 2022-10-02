b87dff899afbef579a6420c58f97296036bc9160
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 10:32:37 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ async (req,res) => {
     const {username, email, password} = req.body;
     const errors = validationResult(req);
     if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
-    if(!uniqueCredentials(email)) return res.status(400).json({error: uniqueCredentials(email)});
+    if(!uniqueCredentials(email)) return res.status(400).json({error: "Email already exists"});
     createUser(username,email,password);
     res.json({status: "ok"});
 })
