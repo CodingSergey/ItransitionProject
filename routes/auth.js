c02b1d6672a7a4f9acecf5f2ec99b42ffabf84fb
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 08:35:30 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.send({status:"ok"});
+    res.send({status:"cock"});
 })
 
 module.exports = router;
