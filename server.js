3fa5b699721a63261203245eb82a6e71b01bd283
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 08:27:52 +0200

    Testing

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req,res)=>{
-    console.log("requested");
-    res.send({status:"cock"});
-})
  
 app.listen(port, ()=>{
     console.log("Listening on port:", port);
