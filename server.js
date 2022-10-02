1f9c8c370ac25555265b1e67779e43dd4685022a
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 08:31:46 +0200

    Testing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const auth  = require("routes/auth");
 let port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
- 
+app.use("/", auth);
 app.listen(port, ()=>{
     console.log("Listening on port:", port);
 });
