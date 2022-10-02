a0498a4e40b252d2154cc059c7d5f938a17f31a9
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 10:44:49 +0200

    Testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const {body, validationResult} = require("express-validator");
 const {createUser, uniqueCredentials} = require("../authorization/validation");
+const Account = require("../models/Account");
 const router = express.Router();
 
 router.post("/register", 
     body("email").isEmail(),
     body("password").isLength({min: 6}),
 async (req,res) => {
-    const {username, email, password} = req.body;
+    const {_username, _email, _password} = req.body;
     const errors = validationResult(req);
     if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
-    if(! await uniqueCredentials(email)) return res.status(400).json({error: "Email already exists"});
-    const user = createUser(username,email,password);
+    if(Account.exists({email: _email})) return res.status(400).json({error: "Email already exists"});
+    const user = createUser(_username,_email,_password);
     return res.json({status: "ok", user: user});
 })
 module.exports = router;
