9b33612cfa92d169167c0f6cb23c1d86e514c827
CodingSergey <a.b.games.business@gmail.com>
2022-10-02 09:31:04 +0200

    Testing

--- authorization/validation.js
+++ authorization/validation.js
@@ -7,3 +7,5 @@ function uniqueCredentials(username,email) {
     const acc = Account.find(acc => acc.email==email || acc.username == username);
     if(acc) return false;
 }
+module.exports.createUser = createUser;
+module.exports.uniqueCredentials = uniqueCredentials;
