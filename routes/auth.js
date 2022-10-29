872098b4b2c7fb836ee00a4444b47850b0c1353b
CodingSergey <a.b.games.business@gmail.com>
2022-10-29 20:28:55 +0200

    new

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,7 @@ router.post("/exists", async (req,res)=> {
     const {_email,_username} = req.body;
     Account.exists({email: _email}, (err,result)=> {
         if(err) res.send(err)
-        else res.send(result?result:false)
+        else res.send(result)
     })
 })
 
