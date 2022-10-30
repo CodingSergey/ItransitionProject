80500a904668804586ae06a5024bf1f38747e918
CodingSergey <a.b.games.business@gmail.com>
2022-10-30 18:53:35 +0100

    Maybe complete idk

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,8 @@ async (req,res) => {
 router.post("/login", async (req,res) => {
     const {_email, _password} = req.body;
     const user = Account.find({email: _email});
-    return res.send({_user:user});
+    if(user) res.send("exists");
+    else res.send("not exists");
     const _token = jwt.sign({username: user.username, exp: Date.now()+ 30 * 60000}, "sodposajfspfsvfaoxjq28343r4fsd");
     return res.json({status: "ok", token: _token});
 });
