b509bd4c0755265406d992544263d8614d837ff9
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 12:51:02 +0100

    authcheck

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
+  const token = authHeader;
 
   if (token == null) return res.send({auth: false, _token: token});
 
