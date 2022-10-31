754db057a752152d1dd785a71e7bb0a51af0972c
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 12:48:55 +0100

    authcheck

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (token == null) return res.sendStatus(401)
+  if (token == null) return res.send({auth: false});
 
   jwt.verify(token,"sodposajfspfsvfaoxjq28343r4fsd", (err, user) => {
     console.log(err);
