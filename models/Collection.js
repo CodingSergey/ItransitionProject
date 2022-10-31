76728e89f326fbea307a51e89dcc9ea8d048bbc9
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 12:40:53 +0100

    authcheck

--- models/Collection.js
+++ models/Collection.js
@@ -1,4 +1,5 @@
-import { model, Schema } from "mongoose";
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CollectionSchema= new Schema({
     name: {type: String, required: true},
