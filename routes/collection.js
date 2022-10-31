a4ff1736f136cc874f07739367a579236991b8a9
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 17:27:15 +0100

    authcheck

--- routes/collection.js
+++ routes/collection.js
@@ -6,7 +6,7 @@ const Collection = require("../models/Collection");
 router.post("/addcollection", authorize, async (req,res)=> {
     const {_name,_description,_topic,_author}=req.body;
     const c = Collection.find({name: _name, author: _author}).count();
-    if(c) return res.send("exists");
+    if(c>0) return res.send("exists");
     await Collection.create({
         name:_name,
         description: _description,
