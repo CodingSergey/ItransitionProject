98f2e44f99728d9931135c628e27a06ee2ce9ce2
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 23:28:59 +0100

    authcheck

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,10 @@ router.delete("/deleteuser/:id", authorize, async(req,res)=> {
 })
 router.put("/toggleblock/:id", authorize, async(req,res)=> {
     const id= req.params["id"];
-    Account.findByIdAndUpdate({_id: id}, {blocked: !blocked});  
+    Account.findById(id, function(err,account) {
+        account.blocked= !account.blocked;
+        account.save();
+    }) 
+    res.send({status: "ok"});
 })
 module.exports=router;
