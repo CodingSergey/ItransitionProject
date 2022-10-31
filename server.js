2eefe591575f9f89ad4a68fc5d4ab81eba11efd9
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 21:29:25 +0100

    authcheck

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const auth  = require("./routes/auth");
 const mongoose = require("mongoose");
 const collection=require("./routes/collection");
+const users = require("./routes/users");
 mongoose.connect("mongodb+srv://alex:1234@cluster0.0nmnmgi.mongodb.net/project?retryWrites=true&w=majority")
 
 let port = process.env.PORT || 8080;
@@ -10,9 +11,10 @@ let port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "https://master--beamish-travesseiro-0c405b.netlify.app",}));
+app.use(cors({origin: "*",}));
 app.use("/auth", auth);
 app.use("/collection", collection);
+app.use("/users",users);
 app.listen(port, ()=>{
     console.log("Listening on port:", port);
 });
