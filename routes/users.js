2bc6062efce1506cc9deac5df6bae2f2d82449fa
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 21:27:21 +0100

    authcheck

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Account=require("../models/Account");
-router.get("/allUsers", async (req,res) => {
+const authorize = require("../middleware/authorization");
+router.get("/allUsers",authorize, async (req,res) => {
     const users = await Account.find().lean();
     res.send(users);
 });
