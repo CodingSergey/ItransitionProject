4afa71538f914b1bd1ef2a9e27603b4b775e9aae
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 17:16:24 +0100

    authcheck

--- models/Collection.js
+++ models/Collection.js
@@ -5,7 +5,8 @@ const CollectionSchema= new Schema({
     name: {type: String, required: true},
     description: {type: String, required:true},
     author:{type: String, required:true},
-    topic:{type: String, required:true}
+    topic:{type: String, required:true},
+    date:{type: Date, required:true}
 })
 
 const Collection = mongoose.model("Collection", CollectionSchema);
