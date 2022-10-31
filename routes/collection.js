00f00e593d70163fd97edd3b9d31957935068c9b
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 12:58:33 +0100

    authcheck

--- routes/collection.js
+++ routes/collection.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const authorize = require("../middleware/authorization");
 const Collection = require("../models/Collection");
 router.post("/addcollection", authorize, async (req,res)=> {
-    const {_name,_description,_topic}=req.body;
+    const {_name,_description,_topic,_author}=req.body;
     const c = Collection.findOne({name: _name}).lean();
     if(c.name) return res.json("exists");
     await Collection.create({
         name:_name,
         description: _description,
-        topic: _topic
+        topic: _topic,
+        author: _author
     })
     res.send({status: "ok"});
 })
